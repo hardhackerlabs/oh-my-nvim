ce57473d68583508ebf6bb608f7c7203e903d930
Skoo Wu <skoowu.fancy@gmail.com>
2023-05-13 23:03:52 +0800

    added automatic setup lsp server
    Signed-off-by: Skoo Wu <skoowu.fancy@gmail.com>

--- lua/plugins/autocmp.lua
+++ lua/plugins/autocmp.lua
@@ -1,5 +1,10 @@
 return {
-    {'hrsh7th/nvim-cmp', config = function() require('plugins/autocmp/config')() end},
+    {
+        'hrsh7th/nvim-cmp',
+        config = function()
+            require('plugins/autocmp/config')()
+        end
+    },
     {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/cmp-buffer'},
     {'hrsh7th/cmp-path' },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,4 @@
 return {
-    {'neovim/nvim-lspconfig', config = function() require('plugins/lspconfig/config')() end},
     {
         "williamboman/mason.nvim",
         config = function() 
@@ -9,17 +8,38 @@ return {
                         package_installed = "✓",
                         package_pending = "➜",
                         package_uninstalled = "✗"
-                    }
+                    },
+                    border = "rounded",
                 }
             })
         end,
     },
+
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup()
+            require("mason-lspconfig").setup_handlers {
+                -- The first entry (without a key) will be the default handler
+                -- and will be called for each installed server that doesn't have
+                -- a dedicated handler.
+                function (server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {}
+                end,
+                -- Next, you can provide a dedicated handler for specific servers.
+                -- For example, a handler override for the `rust_analyzer`:
+                -- ["rust_analyzer"] = function ()
+                --     require("rust-tools").setup {}
+                -- end
+            }
         end,
     },
+
+    {
+        'neovim/nvim-lspconfig',
+        config = function() require('plugins/lspconfig/config')() end,
+    },
+
     {
         'tami5/lspsaga.nvim',
         config = function()
