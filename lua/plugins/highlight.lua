2a2d27f21ee2e82323d8d638aad43bd9cd3bdcb0
skoowu <skoowu.fancy@gmail.com>
2023-06-02 09:25:22 +0800

    added goyo plugin for markdown writing

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -28,8 +28,7 @@ return {
 			})
 
 			-- let it to use 'markdown' parser for mdx filetype.
-			local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
-			ft_to_parser.mdx = "markdown"
+			vim.treesitter.language.register('markdown', 'mdx')
 		end,
 	},
 
--- misc/recommended/markdown.lua
+++ misc/recommended/markdown.lua
@@ -1,6 +1,43 @@
 return {
-    {
-        "iamcco/markdown-preview.nvim",
-        build = function() vim.fn["mkdp#util#install"]() end,
-    },
+	{
+		"iamcco/markdown-preview.nvim",
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	{
+		"junegunn/goyo.vim",
+		dependencies = {
+			{
+				"junegunn/limelight.vim",
+				init = function()
+					vim.g.limelight_conceal_guifg = "#938AAD"
+				end,
+			},
+		},
+		config = function()
+			-- Enter/Leave goyo markdown mode, so hide/unhide the statusline support by lualine
+			goyo_enter = function()
+				require("lualine").hide()
+				require("barbecue.ui").toggle(false)
+				vim.cmd([[
+				Limelight
+				Gitsigns toggle_signs
+				hi CursorLine guibg=NONE
+				]])
+			end
+			goyo_leave = function()
+				require("lualine").hide({ unhide = true })
+				require("barbecue.ui").toggle(true)
+				vim.cmd([[
+				Limelight!
+				Gitsigns toggle_signs
+				]])
+			end
+			vim.cmd([[
+            autocmd! User GoyoEnter nested lua goyo_enter()
+            autocmd! User GoyoLeave nested lua goyo_leave()
+            ]])
+		end,
+	},
 }
