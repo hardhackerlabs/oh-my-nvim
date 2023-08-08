7442d22b9ab71f399dcf559c6e7a3024f96bf270
rikkaka <793329010@qq.com>
2023-08-08 17:42:28 +0800

    install and specify LuaSnip engine

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -30,6 +30,8 @@ return {
 	{ "hrsh7th/cmp-path" },
 	{ "hrsh7th/cmp-cmdline" },
 
+	{ "L3MON4D3/LuaSnip" }, -- install the LuaSnip engine
+
 	{ "onsails/lspkind.nvim" }, -- icons in autocomplete source
 
 	-- todo comments
