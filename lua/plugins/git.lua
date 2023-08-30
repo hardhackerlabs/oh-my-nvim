38245961a3aab896bdef070c1361ca1506561c9f
Zhizhen He <hezhizhen.yi@gmail.com>
2023-08-30 10:34:14 +0800

    display current line blame

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,6 +4,7 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("gitsigns").setup({
+				current_line_blame = true,
 				preview_config = {
 					border = "rounded",
 				},
