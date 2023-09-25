d01de4d8874ded65728121dc8956af3a45bc2251
Tangs <915494737@qq.com>
2023-09-25 23:22:30 +0800

    install romgrk/nvim-treesitter-context
    install nvim-telescope/telescope-file-browser.nvim

---
+++ lua/my_plugins/coding.lua
@@ -0,0 +1,23 @@
+return {
+	{
+    "romgrk/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup{
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        throttle = true, -- Throttles plugin updates (may improve performance)
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+          -- For all filetypes
+          -- Note that setting an entry here replaces all other patterns for this entry.
+          -- By setting the 'default' entry below, you can control which nodes you want to
+          -- appear in the context window.
+          default = {
+            'class',
+            'function',
+            'method',
+          },
+        },
+      }
+    end
+  },
+}
--- lua/plugins/find.lua
+++ lua/plugins/find.lua
@@ -14,8 +14,22 @@ return {
 							yaml = true,
 						},
 					},
+					-- file_browser = {
+					-- 	theme = "ivy",
+					-- 	-- disables netrw and use telescope-file-browser in its place
+					-- 	hijack_netrw = true,
+					-- 	mappings = {
+					-- 		["i"] = {
+					-- 			-- your custom insert mode mappings
+					-- 		},
+					-- 		["n"] = {
+					-- 			-- your custom normal mode mappings
+					-- 		},
+					-- 	},
+					-- },
 				},
 			})
+			require("telescope").load_extension "file_browser"
 
 			local option = { noremap = true, silent = true }
 
@@ -25,7 +39,8 @@ return {
 			vim.keymap.set("n", keys.live_grep, builtin.live_grep, option)
 			vim.keymap.set("n", keys.search_cursor, builtin.grep_string, option)
 			vim.keymap.set("n", keys.find_buffer, builtin.buffers, option)
-			vim.keymap.set('n', '<Leader>aa', require'telescope'.extensions.aerial.aerial, option)
+			vim.keymap.set("n", "<Leader>aa", require("telescope").extensions.aerial.aerial, option)
+			vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>", option)
 		end,
 	},
 }
