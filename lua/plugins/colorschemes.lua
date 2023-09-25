e4db7613185804220e21ded6cc44fb30c0c37b67
Tangs <915494737@qq.com>
2023-09-26 00:48:13 +0800

    fix hl after hardhacker_custom_highlights

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -9,6 +9,13 @@ return {
 			vim.g.hardhacker_hide_tilde = 1
 			vim.g.hardhacker_keyword_italic = 1
 			vim.cmd("colorscheme hardhacker")
+			vim.g.hardhacker_custom_highlights = {
+				-- [[hi! link TreesitterContextBottom Type]],
+				[[hi TreesitterContextBottom gui=underline guisp=Grey]],
+				[[hi! link TreesitterContext Type]],
+				[[hi! link TreesitterContextLineNumber Type]],
+				[[hi! link TreesitterContextSeparator Type]],
+			}
 		end,
 	},
 	{
@@ -18,5 +25,10 @@ return {
 		config = function()
 			-- vim.cmd("colorscheme tokyonight-storm")
 		end,
+	},
+	{
+		"mhartington/oceanic-next",
+		priority = 1000,
+		--	lazy = lvim.colorscheme ~= "OceanicNext",
 	},
 }
