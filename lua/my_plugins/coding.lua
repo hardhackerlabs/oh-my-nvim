a9942ccd76627239ec63cba8e21440510826ba12
Tangs <915494737@qq.com>
2023-11-18 16:03:23 +0800

    ' install kylechui/nvim-surround '

--- lua/my_plugins/coding.lua
+++ lua/my_plugins/coding.lua
@@ -154,6 +154,16 @@ return {
 		end,
 	},
 	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
+	},
+	{
 		"AckslD/swenv.nvim",
 	},
 }
