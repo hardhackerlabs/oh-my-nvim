76bd3698ee5d3bac1e37a5f67bf4482495fe7438
tangs <tangsai_2@live.com>
2023-12-25 20:57:04 +0800

    enable mousemodel in win11

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -2,8 +2,10 @@ if vim.g.neovide then
 	-- Put anything you want to happen only in Neovide here
 	vim.o.guifont = "VictorMono Nerd Font:h12" -- text below applies for VimScript
 	vim.g.neovide_cursor_vfx_mode = "pixiedust"
-	vim.g.neovide_fullscreen = true
-	-- vim.o.mouse = "a"
+	-- vim.g.neovide_fullscreen = true
+	if vim.fn.has("win64") then
+		vim.o.mouse = "a"
+	end
 	vim.g.neovide_theme = "dark"
 	-- vim.g.neovide_floating_shadow = true
 	-- vim.g.neovide_floating_z_height = 10
--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,9 +1,13 @@
 return {
 	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme kanagawa")
+		end,
+	},
+	{
 		"Mofiqul/dracula.nvim",
 		priority = 1000,
-        config = function()
-            vim.cmd("colorscheme dracula")
-        end,
 	},
 }
