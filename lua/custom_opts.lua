c7e42bb5a14cc9105bcb23acf92031b0f432ee93
tangs <tangsai_2@live.com>
2023-12-25 21:02:59 +0800

    perf slanted tabs

--- lua/custom_opts.lua
+++ lua/custom_opts.lua
@@ -1,6 +1,6 @@
 return {
 	file_explorer_title = "🥷 File Explorer",
-	tab_style = "thin", -- options: thin, thick, slope, slant
+	tab_style = "slant", -- options: thin, thick, slope, slant
 	terminal_size = 0.7, -- control the float terminal only
 	window_transparency = 0,
 	auto_open_outline = false, -- options: true, false
